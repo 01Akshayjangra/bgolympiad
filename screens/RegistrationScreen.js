e78afbb44f800e022afdf81311e762c4578ed364
01Akshayjangra <akshayjangra225@gmail.com>
2024-04-09 12:42:10 +0530

    changes

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image, Platform } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; 
-import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 const RegistrationScreen = ({ navigation }) => {
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
@@ -13,80 +20,112 @@ const RegistrationScreen = ({ navigation }) => {
   const handleContinue = () => {
     if (selectedOption) {
       // Navigate to registration form based on selected option
-      console.log('Selected option:', selectedOption);
+      console.log("Selected option:", selectedOption);
 
       // navigation.navigate('RegistrationForm', { category: selectedOption });
     } else {
       // Show error message, no option selected
-      console.log('Please select an option');
+      console.log("Please select an option");
     }
   };
 
   return (
     <View style={styles.container}>
-      <LinearGradient colors={['#AA336A', '#FF0000']} style={styles.header}>
+      <LinearGradient colors={["#AA336A", "#FF0000"]} style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionicons name="arrow-back" size={24} color="white" style={styles.backIcon} />
+          <Ionicons
+            name="arrow-back"
+            size={24}
+            color="white"
+            style={styles.backIcon}
+          />
         </TouchableOpacity>
         <Text style={styles.headerTitle}>Register</Text>
-        <Image source={require('../assets/icon.webp')} style={styles.logoimg} />
+        <Image source={require("../assets/icon.webp")} style={styles.logoimg} />
       </LinearGradient>
       <Text style={styles.title}>Select Registration Type</Text>
       <View style={styles.cardContainer}>
         <TouchableOpacity
-          style={[styles.card, selectedOption === 'student' && styles.selectedCard]}
-          onPress={() => handleOptionSelect('student')}
+          style={[
+            styles.card,
+            selectedOption === "student" && styles.selectedCard,
+          ]}
+          onPress={() => handleOptionSelect("student")}
         >
           <View style={styles.radioButton}>
-            {selectedOption === 'student' ? (
+            {selectedOption === "student" ? (
               <Ionicons name="radio-button-on" size={24} color="blue" />
             ) : (
               <Ionicons name="radio-button-off" size={24} color="blue" />
             )}
           </View>
-          <Image source={require('../assets/student_icon.png')} style={styles.icon} />
+          <Image
+            source={require("../assets/student_icon.png")}
+            style={styles.icon}
+          />
           <Text style={styles.cardText}>Register as a Student</Text>
         </TouchableOpacity>
+
         <TouchableOpacity
-          style={[styles.card, selectedOption === 'teacher' && styles.selectedCard]}
-          onPress={() => handleOptionSelect('teacher')}
+          style={[
+            styles.card,
+            selectedOption === "teacher" && styles.selectedCard,
+          ]}
+          onPress={() => handleOptionSelect("teacher")}
         >
           <View style={styles.radioButton}>
-            {selectedOption === 'teacher' ? (
+            {selectedOption === "teacher" ? (
               <Ionicons name="radio-button-on" size={24} color="blue" />
             ) : (
               <Ionicons name="radio-button-off" size={24} color="blue" />
             )}
           </View>
-          <Image source={require('../assets/student_icon.png')} style={styles.icon} />
+          <Image
+            source={require("../assets/student_icon.png")}
+            style={styles.icon}
+          />
           <Text style={styles.cardText}>Register as a Teacher</Text>
         </TouchableOpacity>
+
         <TouchableOpacity
-          style={[styles.card, selectedOption === 'principal' && styles.selectedCard]}
-          onPress={() => handleOptionSelect('principal')}
+          style={[
+            styles.card,
+            selectedOption === "principal" && styles.selectedCard,
+          ]}
+          onPress={() => handleOptionSelect("principal")}
         >
           <View style={styles.radioButton}>
-            {selectedOption === 'principal' ? (
+            {selectedOption === "principal" ? (
               <Ionicons name="radio-button-on" size={24} color="blue" />
             ) : (
               <Ionicons name="radio-button-off" size={24} color="blue" />
             )}
           </View>
-          <Image source={require('../assets/student_icon.png')} style={styles.icon} />
+          <Image
+            source={require("../assets/student_icon.png")}
+            style={styles.icon}
+          />
           <Text style={styles.cardText}>Register as a Principal</Text>
         </TouchableOpacity>
+
         <TouchableOpacity
-          style={[styles.card, selectedOption === 'other' && styles.selectedCard]}
-          onPress={() => handleOptionSelect('other')}
+          style={[
+            styles.card,
+            selectedOption === "other" && styles.selectedCard,
+          ]}
+          onPress={() => handleOptionSelect("other")}
         >
           <View style={styles.radioButton}>
-            {selectedOption === 'other' ? (
+            {selectedOption === "other" ? (
               <Ionicons name="radio-button-on" size={24} color="blue" />
             ) : (
               <Ionicons name="radio-button-off" size={24} color="blue" />
             )}
           </View>
-          <Image source={require('../assets/student_icon.png')} style={styles.icon} />
+          <Image
+            source={require("../assets/student_icon.png")}
+            style={styles.icon}
+          />
           <Text style={styles.cardText}>Register Other</Text>
         </TouchableOpacity>
       </View>
@@ -100,12 +139,12 @@ const RegistrationScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   header: {
     top: 40,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingHorizontal: 20,
     paddingVertical: 10,
     height: 80,
@@ -115,38 +154,38 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
+    fontWeight: "bold",
+    color: "white",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
     marginTop: 90,
-    textAlign: 'center',
+    textAlign: "center",
   },
   cardContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 20,
   },
   card: {
-    backgroundColor: '#f0f0f0',
+    backgroundColor: "#f0f0f0",
     borderRadius: 10,
-    width: '80%', // Set width to 80% for long-width card
+    width: "80%", // Set width to 80% for long-width card
     aspectRatio: 4, // Adjust aspectRatio according to your design
     height: 700, // Add height to the card
-    flexDirection: 'row', // Align icon and text horizontally
-    alignItems: 'center',
+    flexDirection: "row", // Align icon and text horizontally
+    alignItems: "center",
     marginVertical: 10, // Adjust margin
     borderWidth: 2,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     paddingHorizontal: 10, // Add horizontal padding
   },
   selectedCard: {
-    borderColor: 'blue',
+    borderColor: "blue",
   },
   radioButton: {
     marginRight: 10,
@@ -158,22 +197,22 @@ const styles = StyleSheet.create({
   },
   cardText: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   continueButton: {
-    backgroundColor: 'darkblue',
+    backgroundColor: "darkblue",
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 30,
-    alignItems: 'center',
-    alignSelf: 'center',
+    alignItems: "center",
+    alignSelf: "center",
     marginBottom: 20,
-    width: '80%',
+    width: "80%",
   },
   continueButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   logoimg: {
     width: 60,
